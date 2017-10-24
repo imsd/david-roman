acb3c3f1e12f877bc4a263b4af22011fd693e054
David Roman <david.roman@mail.citytech.cuny.edu>
2017-10-24 19:43:22 -0400

    Add files via upload

---
+++ Unity_Proj1/Assets/Unity_Project1/Follow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow: MonoBehaviour {
+	public Transform target;
+	public float smooth= 5.0f;
+	void  Update (){
+		transform.position = Vector3.Lerp (
+			transform.position, target.position,
+			Time.deltaTime * smooth);
+	} 
+
+} 
